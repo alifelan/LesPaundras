ff795df486e1510319e98ade79166b76e7e9d718
raulherreras <raul_ehs@hotmail.com>
2019-04-18 13:49:59 -0500

    Add button and textView for generating travels in LoginActivity, create NavbarActivity with navigation bar

---
+++ app/src/main/java/com/example/taxiunico_lespaundras/NavbarActivity.kt
@@ -0,0 +1,41 @@
+package com.example.taxiunico_lespaundras
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class NavbarActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                textMessage.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_add_travel -> {
+                textMessage.setText(R.string.add_travel)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navbar)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+}
