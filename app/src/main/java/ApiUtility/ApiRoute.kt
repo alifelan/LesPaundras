e327762dc1e44e24c9411ee3cd78c0a21fc3b2b1
José Flores <josecruzflores.fl@gmail.com>
2019-04-25 18:56:24 -0500

    Add ApiRoute class and add login route

---
+++ app/src/main/java/ApiUtility/ApiRoute.kt
@@ -0,0 +1,50 @@
+package ApiUtility
+
+import android.content.Context
+import com.android.volley.Request
+import org.json.JSONObject
+
+
+sealed class ApiRoute {
+
+    val timeOut: Int
+        get() {
+            return 3000
+        }
+
+    val baseUrl: String
+        get() {
+            return "https://taxi-unico-api.herokuapp.com/"
+        }
+
+    data class Login(var email: String, var password:String, var ctx: Context): ApiRoute()
+
+    val url: String
+        get() {
+            return "$baseUrl/${when (this@ApiRoute) {
+                is Login -> "login"
+            }}"
+        }
+    val httpMethod: Int
+        get() {
+            return when (this) {
+                is Login -> Request.Method.GET
+            }
+        }
+
+    val params: JSONObject
+        get() {
+            return when (this) {
+                is Login -> JSONObject()
+            }
+        }
+
+    val headers: HashMap<String, String>
+        get() {
+            val map: HashMap<String, String> = hashMapOf()
+            map["Accept"] = "application/json"
+            map["Content-Type"] = "application/json"
+            return map
+        }
+
+}
