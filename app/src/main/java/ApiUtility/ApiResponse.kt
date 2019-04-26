365f4d4ed64b25f98602c151b5be47ac5f6b5ffa
José Flores <josecruzflores.fl@gmail.com>
2019-04-25 20:01:15 -0500

    Change ApiResponse to receive JSONObject instead of String

--- app/src/main/java/ApiUtility/ApiResponse.kt
+++ app/src/main/java/ApiUtility/ApiResponse.kt
@@ -1,39 +1,19 @@
 package ApiUtility
 
 import org.json.JSONObject
-import org.json.JSONTokener
 
-class ApiResponse(response: String) {
+class ApiResponse(json: JSONObject) {
 
-    var success: Boolean = false
     var message: String = ""
-    var json: JSONObject = JSONObject()
+    var success: Boolean = true
 
     private val data = "data"
     private val msg = "error"
 
     init {
-        try {
-            val jsonToken = JSONTokener(response).nextValue()
-            if (jsonToken is JSONObject) {
-                json = JSONObject(response)
-
-                message = if (json.has(msg)) {
-                    json.getJSONObject(msg).getString("message")
-                } else {
-                    "An error occurred while processing the response"
-                }
-
-                if (json.optJSONObject(data) != null) {
-                    json = json.getJSONObject(data)
-                    success = true
-                } else {
-                    success = false
-                }
-
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
+        if(json.has("message")) {
+            message = json.getString("message")
+            success = false
         }
     }
 
