e974c67a02815c6d5a003c012fa6caa748be738d
José Flores <josecruzflores.fl@gmail.com>
2019-04-25 21:00:24 -0500

    Change ApiResponse to hold an JSONObject

--- app/src/main/java/ApiUtility/ApiResponse.kt
+++ app/src/main/java/ApiUtility/ApiResponse.kt
@@ -2,7 +2,7 @@ package ApiUtility
 
 import org.json.JSONObject
 
-class ApiResponse(json: JSONObject) {
+class ApiResponse(public var json: JSONObject) {
 
     var message: String = ""
     var success: Boolean = true
