80d76efa5ae72e167934db0dcfbc43a3bdb02727
José Flores <josecruzflores.fl@gmail.com>
2019-04-28 16:19:27 -0500

    Complete user registration and direction to next activity

--- app/src/main/java/com/example/taxiunico_lespaundras/RegisterActivity.kt
+++ app/src/main/java/com/example/taxiunico_lespaundras/RegisterActivity.kt
@@ -1,12 +1,82 @@
 package com.example.taxiunico_lespaundras
 
+import ApiUtility.ApiClient
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
+import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.email
+import kotlinx.android.synthetic.main.activity_register.email_register_button
+import kotlinx.android.synthetic.main.activity_register.login_form
+import kotlinx.android.synthetic.main.activity_register.login_progress
+import kotlinx.android.synthetic.main.activity_register.password
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        email_register_button.setOnClickListener {
+            showProgress(true)
+            ApiClient(applicationContext).createUser(name.text.toString(), email.text.toString(), password.text.toString(), card.text.toString()) { user, success, message ->
+                showProgress(false)
+                Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+                if(success) {
+                    startNavbarActivity(email.text.toString())
+                }
+            }
+        }
+    }
+
+    private fun startNavbarActivity(email: String) {
+        val intent = Intent(this, NavbarActivity::class.java)
+        intent.putExtra(LoginActivity.EMAIL, email)
+        startActivity(intent)
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private fun showProgress(show: Boolean) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+
+            login_form.visibility = if (show) View.GONE else View.VISIBLE
+            login_form.animate()
+                .setDuration(shortAnimTime)
+                .alpha((if (show) 0 else 1).toFloat())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        login_form.visibility = if (show) View.GONE else View.VISIBLE
+                    }
+                })
+
+            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+            login_progress.animate()
+                .setDuration(shortAnimTime)
+                .alpha((if (show) 1 else 0).toFloat())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        login_progress.visibility = if (show) View.VISIBLE else View.GONE
+                    }
+                })
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+            login_form.visibility = if (show) View.GONE else View.VISIBLE
+        }
     }
 }
