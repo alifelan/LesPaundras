d470d7a98607d5e5a8bcf7030e806cad71a29405
José Flores <josecruzflores.fl@gmail.com>
2019-04-27 19:06:03 -0500

    Add User model

--- app/src/main/java/ApiUtility/Models.kt
+++ app/src/main/java/ApiUtility/Models.kt
@@ -1,2 +1,9 @@
 package ApiUtility
 
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String
+) : JSONConvertable
+
