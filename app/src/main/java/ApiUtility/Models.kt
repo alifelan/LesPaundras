ba439e2a35686291e4cbbd6177524b1efea980cb
José Flores <josecruzflores.fl@gmail.com>
2019-04-28 13:29:45 -0500

    Add models related to a bus trip

--- app/src/main/java/ApiUtility/Models.kt
+++ app/src/main/java/ApiUtility/Models.kt
@@ -7,3 +7,19 @@ data class User(
     @SerializedName("email") var email: String = ""
 ) : JSONConvertable
 
+data class Place(
+    @SerializedName("id") var id: String = "",
+    @SerializedName("name") var name: String = "",
+    @SerializedName("state") var state: String = "",
+    @SerializedName("city") var city: String = "",
+    @SerializedName("address") var address: String = ""
+) : JSONConvertable
+
+data class BusTrip(
+    @SerializedName("id") var id: String = "",
+    @SerializedName("origin") var origin: Place,
+    @SerializedName("destination") var destination: Place,
+    @SerializedName("departure_date") var departure_date: String = "",
+    @SerializedName("arrival_date") var arrival_date: String = ""
+)
+
