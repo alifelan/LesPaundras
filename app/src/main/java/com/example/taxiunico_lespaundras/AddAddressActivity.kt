6fe469724c5b0d841ce68f0fb22e2c9e3a070ce7
raulherreras <raul_ehs@hotmail.com>
2019-05-07 18:21:33 -0500

    Added MapView and map button in AddAddressActivity

--- app/src/main/java/com/example/taxiunico_lespaundras/AddAddressActivity.kt
+++ app/src/main/java/com/example/taxiunico_lespaundras/AddAddressActivity.kt
@@ -3,9 +3,18 @@ package com.example.taxiunico_lespaundras
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_add_address.*
 
 class AddAddressActivity : AppCompatActivity() {
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +27,23 @@ class AddAddressActivity : AppCompatActivity() {
         val data = intent.extras
         val srcDest: String = data.getString(AddTripActivity.SRCDEST)
         add_address_text_title.text = "Add " + srcDest + " address"
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.add_address_map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            //googleMap.isMyLocationEnabled = true
+            val location1 = LatLng(13.03,77.60)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+
+            val location2 = LatLng(9.89,78.11)
+            googleMap.addMarker(MarkerOptions().position(location2).title("Madurai"))
+
+
+            val location3 = LatLng(13.00,77.00)
+            googleMap.addMarker(MarkerOptions().position(location3).title("Bangalore"))
+
+        })
     }
 
     // back button
