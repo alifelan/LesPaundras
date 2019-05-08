e60b0810bb65df97cbef45113eaaa92e9f586360
José Flores <josecruzflores.fl@gmail.com>
2019-05-08 00:01:25 -0500

    Ask for address result

--- app/src/main/java/com/example/taxiunico_lespaundras/AddTripActivity.kt
+++ app/src/main/java/com/example/taxiunico_lespaundras/AddTripActivity.kt
@@ -1,7 +1,9 @@
 package com.example.taxiunico_lespaundras
 
+import ApiUtility.Address
 import ApiUtility.ApiClient
 import ApiUtility.BusTrip
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +14,9 @@ import kotlinx.android.synthetic.main.activity_add_trip.*
 class AddTripActivity : AppCompatActivity() {
 
     lateinit var trip: BusTrip
+    var source: Address? = null
+    var destination: Address? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,19 +33,31 @@ class AddTripActivity : AppCompatActivity() {
         add_trip_activity_button_add_src_address.setOnClickListener {
             val addSrcIntent = Intent(this, AddAddressActivity::class.java)
             addSrcIntent.putExtra(SRCDEST, "source")
-            startActivity(addSrcIntent)
+            startActivityForResult(addSrcIntent, TRIP_START)
         }
 
         add_trip_activity_button_add_dest_address.setOnClickListener {
             val addDestIntent = Intent(this, AddAddressActivity::class.java)
             addDestIntent.putExtra(SRCDEST, "destination")
-            startActivity(addDestIntent)
+            startActivityForResult(addDestIntent, TRIP_END)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK) {
+            if(requestCode == TRIP_START) {
+                source = data?.extras?.getParcelable(AddAddressActivity.ADDRESS)
+            } else if(requestCode == TRIP_END) {
+                destination = data?.extras?.getParcelable(AddAddressActivity.ADDRESS)
+            }
         }
     }
 
     companion object {
         const val SRCDEST: String = "srcDest"
         const val CURRENT_TRIP: String ="current"
+        const val TRIP_START: Int = 1
+        const val TRIP_END: Int = 2
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
