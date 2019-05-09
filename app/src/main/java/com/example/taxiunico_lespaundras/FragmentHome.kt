2003043ba353727d9a196278a2d01692d333801b
José Flores <josecruzflores.fl@gmail.com>
2019-05-08 23:57:24 -0500

    Create setInfo function

--- app/src/main/java/com/example/taxiunico_lespaundras/FragmentHome.kt
+++ app/src/main/java/com/example/taxiunico_lespaundras/FragmentHome.kt
@@ -1,6 +1,7 @@
 package com.example.taxiunico_lespaundras
 
 import ApiUtility.ApiClient
+import ApiUtility.TaxiTrip
 import ViewModels.UserViewModel
 import android.arch.lifecycle.ViewModelProviders
 import android.graphics.Color
@@ -34,29 +35,46 @@ class FragmentHome : Fragment() {
         mapFragment = childFragmentManager.findFragmentById(R.id.home_view_map) as SupportMapFragment
         mapFragment.getMapAsync{
             googleMap = it
-            ApiClient(activity?.applicationContext!!).getCurrentOrNextTrip(model.user?.email!!) {trip, current, success, message ->
-                if(success && trip != null) {
-                    val origin = "${trip?.origin?.address},${trip?.origin?.city},${trip?.origin?.state}"
-                    val destination = "${trip?.destination?.address},${trip?.destination?.city},${trip?.destination?.state}"
-                    ApiClient(activity?.applicationContext!!).getDirections(origin, destination){route, success, message ->
-                        if(success) {
-                            val lineOptions = PolylineOptions()
-                            lineOptions.addAll(route?.points)
-                            lineOptions.width(6f)
-                            lineOptions.color(ContextCompat.getColor(activity?.applicationContext!!, R.color.gray))
-                            googleMap.addPolyline(lineOptions)
-                            val oLatLng = LatLng(trip?.origin?.latitue!!, trip.origin.longitude)
-                            googleMap.addMarker(MarkerOptions().position(oLatLng).title(trip.origin.name))
-                            val dLatLng = LatLng(trip.destination.latitue, trip.destination.longitude)
-                            googleMap.addMarker(MarkerOptions().position(dLatLng).title(trip.destination.name))
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(oLatLng, 12f))
-                        } else {
-                            Toast.makeText(activity?.applicationContext!!, message, Toast.LENGTH_SHORT).show()
-                        }
+            setRoute()
+        }
+    }
+
+    private fun setInfo(trip: TaxiTrip, current: Boolean) {
+        if(!current) {
+            home_text_title.text = "Next trip"
+        }
+        home_text_src.text = trip.origin.name
+        home_text_dest.text = trip.destination.name
+        home_text_driver_info_name.text = trip.taxi.driverName
+        home_text_driver_info_brand.text = trip.taxi.brand
+        home_text_driver_info_plates.text = trip.taxi.plate
+        home_text_driver_info_taxiNum.text = trip.taxi.taxi_number
+    }
+
+    private fun setRoute() {
+        ApiClient(activity?.applicationContext!!).getCurrentOrNextTrip(model.user?.email!!) {trip, current, success, message ->
+            if(success && trip != null) {
+                setInfo(trip, current)
+                val origin = "${trip?.origin?.address},${trip?.origin?.city},${trip?.origin?.state}"
+                val destination = "${trip?.destination?.address},${trip?.destination?.city},${trip?.destination?.state}"
+                ApiClient(activity?.applicationContext!!).getDirections(origin, destination){route, success, message ->
+                    if(success) {
+                        val lineOptions = PolylineOptions()
+                        lineOptions.addAll(route?.points)
+                        lineOptions.width(6f)
+                        lineOptions.color(ContextCompat.getColor(activity?.applicationContext!!, R.color.gray))
+                        googleMap.addPolyline(lineOptions)
+                        val oLatLng = LatLng(trip?.origin?.latitue!!, trip.origin.longitude)
+                        googleMap.addMarker(MarkerOptions().position(oLatLng).title(trip.origin.name))
+                        val dLatLng = LatLng(trip.destination.latitue, trip.destination.longitude)
+                        googleMap.addMarker(MarkerOptions().position(dLatLng).title(trip.destination.name))
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(oLatLng, 12f))
+                    } else {
+                        Toast.makeText(activity?.applicationContext!!, message, Toast.LENGTH_SHORT).show()
                     }
-                } else {
-                    Toast.makeText(activity?.applicationContext, message, Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(activity?.applicationContext, message, Toast.LENGTH_SHORT).show()
             }
         }
     }
