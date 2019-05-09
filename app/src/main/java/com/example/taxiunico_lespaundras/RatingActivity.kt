6eb5a3ed6099b5ea1718ccc3d0c92c8874e5b390
José Flores <josecruzflores.fl@gmail.com>
2019-05-09 11:36:18 -0500

    Fill RatingActivity

--- app/src/main/java/com/example/taxiunico_lespaundras/RatingActivity.kt
+++ app/src/main/java/com/example/taxiunico_lespaundras/RatingActivity.kt
@@ -1,14 +1,40 @@
 package com.example.taxiunico_lespaundras
 
+import ApiUtility.ApiClient
+import ApiUtility.Taxi
+import ApiUtility.TaxiTrip
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_rating.*
 
 class RatingActivity : AppCompatActivity() {
 
+    lateinit var trip: TaxiTrip
+    lateinit var email: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rating)
+
+        trip = intent.extras.getParcelable(FragmentHome.TRIP)
+        email = intent.extras.getString(LoginActivity.EMAIL)
+
+        rating_text_driver_info.text = trip.taxi.driverName
+
+        rating_button_rate.setOnClickListener {
+            ApiClient(this@RatingActivity).rateDriver(trip.id, rating_driver_stars.rating) {_, success, message ->
+                if(success) {
+                    val navIntent = Intent(this@RatingActivity, NavbarActivity::class.java).apply {
+                        putExtra(LoginActivity.EMAIL, email)
+                    }
+                    startActivity(navIntent)
+                } else {
+                    Toast.makeText(this@RatingActivity, message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
         
         rating_driver_stars.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->  }
     }
